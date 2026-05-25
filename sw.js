bfdfc6597743ae51c3bb57f9709ce932289f4f86
Thecosmiccat <jefftong30@gmail.com>
2026-05-25 18:28:51 +0100

    Actually fix Browse layout and Vidnest AniList embed IDs (v3.5).
    Previous e4501a6 commit had no file changes. Restore AniList id in player URLs and move Browse into hero.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'catime-v3';
+const CACHE = 'catime-v4';
 const ASSETS = ['./', './index.html', './js/catime-api.js', './js/catime-storage.js', './js/catime-ui.js', './js/catime-features.js'];
 
 function isHtmlRequest(url) {
